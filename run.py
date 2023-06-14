f97e3caa98cff3381d32bdc701070c471ca82b7a
0w3nM <>
2023-06-14 19:21:05 +0000

    Add modules, basic sequence function, user input and validation

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Modules
+import random
+import os
+from time import sleep
+
+sequence_length = 5
+max_number = 9
+
+numbers = []
+
+for sequence_index in range(sequence_length):
+    random_number = random.randint(1, max_number)
+    numbers.append(random_number)
+
+print("Guess the sequence!: ", numbers)
+print("Your screen will be cleared in 5 seconds...")
+sleep(5)
+os.system("clear")
+
+guess = input("Type the order of the numbers: ")
+guessed_sequence = list(map(int, guess.split()))
+
+if guessed_sequence == numbers:
+    print("Correct!")
+else:
+    print("Incorrect! Answer is :", numbers)
