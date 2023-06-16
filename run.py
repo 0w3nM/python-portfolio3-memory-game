7ccd993ccb32936f1e7da39ff05beae90eae964a
0w3nM <>
2023-06-16 22:50:58 +0000

    Add comments, rearrange functions, adjust and add code in functions

--- run.py
+++ run.py
@@ -1,13 +1,22 @@
 # Modules
 from random import shuffle
 import os
-import system
+import sys
 from time import sleep
+
+# Users board
+user_board = [[""] * 9 for i in range(9)]
 
 # Counts the number of gueses user is limited to.
 guess_limit = 0
 # Counts how many answers were correct.
 correct_guesses = 0
+# Length of numbers shown.
+sequence_length = 6
+# Highest number that will be shown.
+max_number = 9
+# Numbers that are shown.
+numbers = []
 
 
 def rules():
@@ -19,43 +28,84 @@ def rules():
     print("Fail more than 3 times and its GAME OVER!")
     
 
-def random_list():
-    sequence_length = 6
-    low_number = 1
-    max_number = 9
-    random_list = random.shuffle(range(low_number, max_number),sequence_length)
-    print(random_list)
+def print_board(board):
+    """
+    Function to print board
+    """
+    print('+') * 9
+    row_number = 1
 
-print ("Guess the sequence: ", random_list)
 
-def user_input(prompt):
-    return input (prompt)
+def random_numbers():
+    """
+    List of randomised numbers.
+    """
+    for sequence_index in range(sequence_length):
+        random_numbers = randint(1, max_number)
+        numbers.append(random_numbers)
+    print("Guess the sequence", numbers)
 
-guess = user_input("Type the order of the numbers: ")
-user_sequence = list(map(int, guess.split()))
 
-if user_sequence == random_list:
-    print("Correct!")
-else:
-    print("Incorrect! Answer is :", random_list)
+def user_input(board):
+    """
+    Allows player to input guess
+    of the sequence.
+    """
+    user_guess = int(input("What was the Sequence ?: "))
 
+    
 while guess_limit < 5:
     guess = int(input())
-    guess_limit =+ 1
-    if guess == random_list:
+    guess_limit = + 1
+    if guess == random_numbers:
         print("CORRECT !!")
-    if guess != random_list:
-        print ("Incorrect!! Sequence is: ", random_list)
+    if guess != random_numbers:
+        print("Incorrect!! Sequence is: ", random_numbers)
         break
 
 while correct_guesses < 5:
     guess = int(input())
-    correct_guesses =+ 1
+    correct_guesses = + 1
     if correct_guesses <= 3:
         print("YOU LOSE !!")
     if correct_guesses >= 3:
         print("YOU WIN!!")
         break
 
+
+def play_game():
+    """
+    Function to start and play game.
+    """
+    # Adds the board for user.
+    print_board(user_board)
+    while True:
+        print(random_numbers)
+        print(user_input)
+        if guess_limit == 5:
+            print(guess_limit)
+            print(correct_guesses)
+            return cont_game()
+
+def cont_game():
+    """
+    Allows the user to keep playing
+    """
+    user_board = [[""] * 9 for i in range(9)]
+    print("Do you wish to continue playing?")
+    answer = input("Y / N").upper()
+    print("")
+    while True:
+        if answer == "Y":
+            play_game()
+        elif answer == "N":
+            print("")
+            print("Thanks for Playing!")
+            sys.exit()
+
+
+
+
 # Print statement showing the final resuilt of the game.
+print("-") * 25
 print(f"Your Total is {correct_guesses} out of {guess_limit}")
