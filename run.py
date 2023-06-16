2e2d8378b7c543f591ee50d4a8395d58004cd9ac
0w3nM <>
2023-06-16 00:56:07 +0000

    Add more modules, update random number generator

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 # Modules
-import random
+from random import shuffle
 import os
+import system
 from time import sleep
 
-# How many numbers appear to the user.
-sequence_length = 5
-# How high the numbers shown will go upto.
-max_number = 9
+# Counts the number of gueses user is limited to.
+guess_limit = 0
+# Counts how many answers were correct.
+correct_guesses = 0
 
 
 def rules():
@@ -16,18 +17,16 @@ def rules():
     print("Welcome to the Memory Game! /n")
     print("The objective of this game is for you to try and guess the sequence")
     print("Fail more than 3 times and its GAME OVER!")
+    
 
-numbers = []
+def random_list():
+    sequence_length = 6
+    low_number = 1
+    max_number = 9
+    random_list = random.shuffle(range(low_number, max_number),sequence_length)
+    print(random_list)
 
-
-for sequence_index in range(sequence_length):
-    random_number = random.randint(1, max_number)
-    numbers.append(random_number)
-
-print("Guess the sequence!: ", numbers)
-print("Your screen will be cleared in 5 seconds...")
-sleep(5)
-os.system("clear")
+print ("Guess the sequence: ", random_list)
 
 guess = input("Type the order of the numbers: ")
 guessed_sequence = list(map(int, guess.split()))
