cbc430b4a9ae807aca33f912092f18f5e1bf0b51
0w3nM <>
2023-06-16 01:11:18 +0000

    Update code in 'user_sequence' if statement. Add 'while' loop on 'guess_limit' and 'correct_guesses'. Add total

--- run.py
+++ run.py
@@ -34,7 +34,28 @@ def user_input(prompt):
 guess = user_input("Type the order of the numbers: ")
 user_sequence = list(map(int, guess.split()))
 
-if user_sequence == numbers:
+if user_sequence == random_list:
     print("Correct!")
 else:
-    print("Incorrect! Answer is :", numbers)
+    print("Incorrect! Answer is :", random_list)
+
+while guess_linit < 5:
+    guess = int(input())
+    guess_limit =+ 1
+    if guess == random_list:
+        print("CORRECT !!")
+    if guess != random_list:
+        print ("Incorrect!! Sequence is: ", random_list)
+        break
+
+while correct_guesses < 5:
+    guess = int(input())
+    correct_guesses =+ 1
+    if correct_guesses <= 3:
+        print("YOU LOSE !!")
+    if correct_guesses >= 3:
+        print("YOU WIN!!")
+        break
+
+# Print statement showing the final resuilt of the game.
+print(f"Your Total is {correct_guesses} out of {guess_limit}")
